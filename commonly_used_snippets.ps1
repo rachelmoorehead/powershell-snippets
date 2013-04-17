995ad30d642d6bbdf161634d97b593a7e687819b
Rachel M <rmoorehe@uga.edu>
2013-04-17 11:18:46 -0400

    WIP - Initialization

---
+++ commonly_used_snippets.ps1
@@ -0,0 +1,28 @@
+﻿<#
+
+    Commonly Used Snippets
+
+#>
+
+### Text Manipulation ###
+
+# Casing
+$GroupName = "test_group_name"
+$DisplayName = (Get-Culture).TextInfo.ToTitleCase($GroupName).Replace("_"," ")
+
+### File Manipulation ###
+
+# Importing CSV
+$Items = Get-Content "file.csv"
+foreach ($Item in $Items){ 
+    
+    $columns = $Item.Split(',') #Or your delimiter
+    $col1 = $columns[0]
+    $col2 = $columns[1] # ..etc..
+
+    <# Do Something #> 
+    
+    }
+
+# Clearing Files for Initialization
+if(Test-Path $log){  Remove-Item $log <# Or Rename or Move, etc.. #> }
